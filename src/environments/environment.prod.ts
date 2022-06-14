499b3817459d8d8d2b4acc3b0917f4ad15779df4
fOx2 <klausbod07@gmail.com>
2022-06-13 19:18:29 -0500

    primer commit modificacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDb_gw8-APEy_mAGGcmdoXvp5U4shrpthM",
+    authDomain: "tesis1-34213.firebaseapp.com",
+    projectId: "tesis1-34213",
+    storageBucket: "tesis1-34213.appspot.com",
+    messagingSenderId: "92509266612",
+    appId: "1:92509266612:web:5dc0f1a06bb51fa2ba2de1"
+  }
 };
